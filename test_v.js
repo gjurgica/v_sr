2896aaa99d32d4bc589ee8e594de1a71b2fafde7
Gjurgica Anastasovska <gurgica78@gmail.com>
2021-11-18 14:40:31 +0100

    l

--- test_v.js
+++ test_v.js
@@ -1,4 +1,20 @@
 function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("popuptext");
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("popuptext");
+}
+function openData() {
+  var div = document.getElementById("content");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
   }
+}
+function openSecondData() {
+  var div = document.getElementById("second-content");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
